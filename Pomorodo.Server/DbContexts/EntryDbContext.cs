b535b476fc6d5b251fb588f90fb133a95bcf3484
Akila Peramatchi <dev.akila001@outlook.com>
2024-03-29 01:05:26 +0530

    Created a Model, DbContext

---
+++ Pomorodo.Server/DbContexts/EntryDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomorodo.Server.Models;
+using System.Collections.Generic;
+
+namespace Pomorodo.Server.DbContexts
+{
+    public class EntryDbContext : DbContext
+    {
+        public EntryDbContext(DbContextOptions<EntryDbContext> options)
+       : base(options)
+        {
+        }
+        public DbSet<Entry> entries { get; set; }
+
+        public EntryDbContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configurationInstance = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory)?.FullName ?? ".")
+                .AddJsonFile("appsettings.json", optional: true)
+                //  .AddJsonFile("appsettings.local.json", optional: true)
+                .Build();
+            string dbConnString = configurationInstance["ConnectionStrings: entrydb"] ?? "";
+            optionsBuilder.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
+            optionsBuilder.UseNpgsql(dbConnString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Pomorodo.Server/Models/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pomorodo.Server.Models
+{
+    [Table("entry_time")]
+    public class Entry
+    {
+        [Key]
+        [Column("entry_id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string? Name { get; set; }
+        [Column("start_time")]
+        public DateTime StartTime { get; set; }
+        [Column("end_time")]
+        public DateTime EndTime { get; set; }
+     
+
+    }
+}
